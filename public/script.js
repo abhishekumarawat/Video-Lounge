8e7212257eaac5031ab9f87bc8da0bf42df6812b
Abhishek <abhishekkumar1998cod@gmail.com>
2021-06-12 23:39:54 +0530

    Add some basic features of video calling and chatting with multiple users.

---
+++ public/script.js
@@ -0,0 +1,195 @@
+const socket = io("/");
+const chatInputBox = document.getElementById("chat_message");
+const all_messages = document.getElementById("all_messages");
+const main__chat__window = document.getElementById("main__chat__window");
+const videoGrid1 = document.getElementById("video-grid1");
+const videoGrid2 = document.getElementById("video-grid2");
+const myVideo = document.createElement("video");
+myVideo.muted = true;
+
+var peer = new Peer(undefined, {
+  path: "/peerjs",
+  host: "/",
+  port: "443",
+});
+
+let myVideoStream;
+const peers = {};
+
+var getUserMedia =
+  navigator.getUserMedia ||
+  navigator.webkitGetUserMedia ||
+  navigator.mozGetUserMedia;
+
+navigator.mediaDevices
+  .getUserMedia({
+    video: true,
+    audio: true,
+  })
+  .then((stream) => {
+    myVideoStream = stream;
+    addVideoStream(myVideo, stream);
+
+    peer.on("call", (call) => {
+      call.answer(stream);
+      const video = document.createElement("video");
+
+      call.on("stream", (userVideoStream) => {
+        addVideoStream(video, userVideoStream);
+      });
+    });
+
+    socket.on("user-connected", (userId) => {
+      connectToNewUser(userId, stream);
+    });
+
+    document.addEventListener("keydown", (e) => {
+      if (e.which === 13 && chatInputBox.value != "") {
+        socket.emit("message", chatInputBox.value);
+        chatInputBox.value = "";
+      }
+    });
+
+    socket.on("createMessage", (msg) => {
+      console.log(msg);
+      let li = document.createElement("li");
+      li.innerHTML = msg;
+      all_messages.append(li);
+      main__chat__window.scrollTop = main__chat__window.scrollHeight;
+    });
+  });
+
+peer.on("call", function (call) {
+  getUserMedia(
+    { video: true, audio: true },
+    function (stream) {
+      call.answer(stream); // Answer the call with an A/V stream.
+      const video = document.createElement("video");
+      call.on("stream", function (remoteStream) {
+        addVideoStream(video, remoteStream);
+      });
+    },
+    function (err) {
+      console.log("Failed to get local stream", err);
+    }
+  );
+});
+
+peer.on("open", (id) => {
+  socket.emit("join-room", ROOM_ID, id);
+});
+
+socket.on("user-disconnected", userID => {
+  if(peers[userID]) peers[userID].close()
+})
+
+// CHAT
+
+const connectToNewUser = (userId, streams) => {
+  var call = peer.call(userId, streams);
+  console.log(call);
+  var video = document.createElement("video");
+  call.on("stream", (userVideoStream) => {
+    console.log(userVideoStream);
+    addVideoStream(video, userVideoStream);
+  });
+  call.on("close", () => {
+    video.remove()
+  })
+
+  peers[userId] = call;
+
+};
+
+const addVideoStream = (videoEl, stream) => {
+  videoEl.srcObject = stream;
+  videoEl.addEventListener("loadedmetadata", () => {
+    videoEl.play();
+  });
+
+  
+  let totalUsers = document.getElementsByTagName("video").length;
+
+  let div = document.createElement('div');
+
+  if(totalUsers<=3){
+    videoGrid1.append(videoEl)
+    if (totalUsers > 1) {
+      for (let index = 0; index < totalUsers; index++) {
+        document.getElementsByTagName("video")[index].style.width =
+          100 / totalUsers + "%";
+          //document.getElementsByTagName("video")[index].className = "col";
+      }
+    }
+  }
+  if(totalUsers>3){
+    //div.classList.add('w-100');
+    videoGrid2.append(videoEl);
+    if (totalUsers > 1) {
+      for (let index = 0; index < totalUsers; index++) {
+        document.getElementsByTagName("video")[index].style.width =
+          100 / totalUsers + "%";
+        document.getElementsByTagName("video")[index].style.maxWidth = 400 + "px";
+          //document.getElementsByTagName("video")[index].className = "col";
+      }
+    }
+  }
+  
+  // if(totalUsers<=3) videoGrid1.append(videoEl);
+  // if(totalUsers>3) videoGrid2.append(videoEl);
+  // if (totalUsers > 1) {
+  //   for (let index = 0; index < totalUsers; index++) {
+  //     document.getElementsByTagName("video")[index].style.width =
+  //       100 / totalUsers + "%";
+  //       //document.getElementsByTagName("video")[index].className = "col";
+  //   }
+  // }
+  
+};
+
+const playStop = () => {
+  let enabled = myVideoStream.getVideoTracks()[0].enabled;
+  if (enabled) {
+    myVideoStream.getVideoTracks()[0].enabled = false;
+    setPlayVideo();
+  } else {
+    setStopVideo();
+    myVideoStream.getVideoTracks()[0].enabled = true;
+  }
+};
+
+const muteUnmute = () => {
+  const enabled = myVideoStream.getAudioTracks()[0].enabled;
+  if (enabled) {
+    myVideoStream.getAudioTracks()[0].enabled = false;
+    setUnmuteButton();
+  } else {
+    setMuteButton();
+    myVideoStream.getAudioTracks()[0].enabled = true;
+  }
+};
+
+const setPlayVideo = () => {
+  const html = `<i class="unmute fa fa-pause-circle"></i>
+  <span class="unmute">Resume Video</span>`;
+  document.getElementById("playPauseVideo").innerHTML = html;
+};
+
+const setStopVideo = () => {
+  const html = `<i class=" fa fa-video-camera"></i>
+  <span class="">Pause Video</span>`;
+  document.getElementById("playPauseVideo").innerHTML = html;
+};
+
+const setUnmuteButton = () => {
+  const html = `<i class="unmute fa fa-microphone-slash"></i>
+  <span class="unmute">Unmute</span>`;
+  document.getElementById("muteButton").innerHTML = html;
+};
+const setMuteButton = () => {
+  const html = `<i class="fa fa-microphone"></i>
+  <span>Mute</span>`;
+  document.getElementById("muteButton").innerHTML = html;
+};
+
+
