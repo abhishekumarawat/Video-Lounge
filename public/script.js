1749955ff5f61304259443182f5714d4fc94c3d9
Abhishek <abhishekkumar1998cod@gmail.com>
2021-06-13 01:10:13 +0530

    Fix bug related the max width and hieght of video

--- public/script.js
+++ public/script.js
@@ -10,8 +10,8 @@ myVideo.muted = true;
 var peer = new Peer(undefined, {
   path: "/peerjs",
   host: "/",
-  port: "443",
-  //port: 3000,
+  //port: "443",
+  port: 3000,
 });
 
 let myVideoStream;
